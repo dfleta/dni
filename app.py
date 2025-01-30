3b70a5fb18425b15726a6655fc908200c86482ca
David Gelpi <gelpiorama@gmail.com>
2025-01-30 15:07:11 +0100

    refactor(app): Definir entry point main
    Encapsular responsabilidades en su funcion correspondiente

--- app.py
+++ app.py
@@ -3,133 +3,158 @@ from src.tablaAsignacion import TablaAsignacion
 from src.cli_colors import Colors
 from src.dni_cif import Dni
 
+LETRAS_NO_PERMITIDAS = ["I", "Ñ", "O", "U"]
 
-### TABLA ASIGNACION ###
 
-tabla = TablaAsignacion()
+def mostrarLetrasNoPermitidas(tabla):
+    for letra_no_permitida in LETRAS_NO_PERMITIDAS:
+        print(f"Letra {letra_no_permitida}: \
+          {tabla.isLetraPermitida(letra_no_permitida)}")
 
-print("\n######     TABLA     ######\n")
 
-# tabla.mostrarTabla()
-print(tabla)
-
-print("\n## ACCESO POR POSICION ##\n")
-
-print(tabla.getLetra(0))  # T
-print(tabla.getLetra(22)) # E
-print(tabla.getLetra(30)) # Excepcion!
-
-print("\n## LETRAS NO PERMITIDAS ##\n")
-
-letrasNoPermitidas = ["I", "Ñ", "O", "U"]
-for letraNoPermitida in letrasNoPermitidas:
-    print(f"Letra {letraNoPermitida}: \
-          {tabla.isLetraPermitida(letraNoPermitida)}")
-
-### Añado casos test INCORRECTOS ALEATORIOS ###
-
-casos_test_letra_prohibida = []
-NUMERO_CASOS = 15
-
-for i in range(1, NUMERO_CASOS + 1):
-    caso = ""
-    for j in range(1, 9):
+def generarCIFLetraNoPermitida(NUMERO_CASOS):
+    CERO_ASCII = 48
+    NUEVE_ASCII = 57
+    LONGITUD_DNI = 8
+    cif_con_letra_prohibida = []
+    for _ in range(NUMERO_CASOS):
+        dni = ""
+        # CIF tiene 8 digito y una letra
+        for _ in range(LONGITUD_DNI):
         # random.randrange(start, stop[, step])
         # numeroAleatorio = random.randint(0, 9)
         # ASCII 48-57 = 0-9
         # generamos un numero aleatorio entre 48 y 57
-        caracterAscii = random.randrange(48, 57 + 1, 1)
-        # convertimos el numero ASCII a caracter.
-        # chr() toma el argumento como codigo ASCII de un caracter
-        caso = caso + chr(caracterAscii)
-    # en la ultima posicion añado una letra NO PERMITIDA
-    # ['I', 'Ñ', 'O', 'U']
-    caso = caso + letrasNoPermitidas[random.randrange(0, 3 + 1, 1)]
-    casos_test_letra_prohibida = casos_test_letra_prohibida + [caso]
-
-print("\n## CASOS TEST LETRA NO PERMITIDA ##\n")
-
-print(casos_test_letra_prohibida)
-
-for dni in casos_test_letra_prohibida:
-    if tabla.calcularLetra(dni[:-1]) == dni[-1]:
-        print(f"{dni} {Colors.OKGREEN.value} OK {Colors.ENDC.value}")
-    else:
-        # print("%s %s" % (dni, Colors.FAIL + "FAIL" + Colors.ENDC))
-        print(f"{dni} {Colors.FAIL.value} FAIL {Colors.ENDC.value}")
+            caracter_ascii = random.randrange(CERO_ASCII, NUEVE_ASCII)
+            # convertimos el numero ASCII a caracter.
+            # chr() toma el argumento como codigo ASCII de un caracter
+            dni += chr(caracter_ascii)
+        # en la ultima posicion añado una letra NO PERMITIDA
+        # ['I', 'Ñ', 'O', 'U']
+        cif = dni + LETRAS_NO_PERMITIDAS[random.randrange(0, \
+                                                    len(LETRAS_NO_PERMITIDAS))]
+        cif_con_letra_prohibida.append(cif)
+    return cif_con_letra_prohibida
 
 
-### DNI ###
+def generar_cifs_aleatorios(numero_casos):
+    LONGITUD_DNI = 8
+    CERO_ASCII = 48
+    DOS_PUNTOS_ASCII = 58
+    dnis_aleatorios = []
+    for _ in range(numero_casos):
+        dni = ""
+        for _ in range(LONGITUD_DNI):
+            # ASCII 48-57 = 0-9    65-90 = A-Z   58 = ":"
+            caracter_ascii = random.randrange(CERO_ASCII, DOS_PUNTOS_ASCII)
+            # convertimos el numero ASCII a caracter.
+            dni += chr(caracter_ascii)
+        # en la ultima posicion añado una letra A-Z
+        cif = dni + chr(random.randrange(65, 90 + 1, 1))
+        dnis_aleatorios.append(cif)
+    return dnis_aleatorios
 
-print("\n\n######     DNI     ######\n")
 
 def prettyFormatter(condition, message):
     print(f"{Colors.OKGREEN.value} {message} {Colors.ENDC.value}"
             if condition
             else f"{Colors.FAIL.value} {message} {Colors.ENDC.value}")
 
-### Casos test DNI ALEATORIOS ###
 
-casos_test = []
-NUMERO_CASOS = 25
+def main():
 
-for i in range(1, NUMERO_CASOS + 1):
-    caso = ""
-    for j in range(1, 9):
-        # random.randrange(start, stop[, step])
-        # numeroAleatorio = random.randint(0, 9)
-        # ASCII 48-57 = 0-9    65-90 = A-Z   58 = ":"
-        # generamos un numero aleatorio entre 48 y 58
-        caracterAscii = random.randrange(48, 58 + 1, 1)
-        # convertimos el numero ASCII a caracter.
-        # chr() toma el argumento como codigo ASCII de un caracter
-        caso = caso + chr(caracterAscii)
-    # en la ultima posicion añado una letra A-Z
-    caso = caso + chr(random.randrange(65, 90 + 1, 1))
-    casos_test = casos_test + [caso]
+    ### TABLA ASIGNACION ###
 
-print("\n## CASOS TEST DNI ALEATORIOS ##\n")
+    tabla = TablaAsignacion()
 
-print(casos_test)
+    print("\n######     TABLA     ######\n")
 
-for testString in casos_test:
-    dni = Dni(testString)
-    print(dni.getDni())
-    dni.checkCIF()
-    # print("dni --->", dni.getNumeroSano())
-    # print("Letra --->", dni.getLetraSana())
-    # print("La letra es", dni.obtenerLetra())
-    prettyFormatter(dni.getNumeroSano(), dni.getDni())
-    prettyFormatter(dni.getLetraSana(), dni.obtenerLetra())
+    # tabla.mostrarTabla()
+    print(tabla)
 
-### Casos test OK ###
+    print("\n## ACCESO POR POSICION ##\n")
 
-CASOS_TEST_OK = [  # casos OK
-    "78484464T",
-    "72376173A",
-    "01817200Q",
-    "95882054E",
-    "63587725Q",
-    "26861694V",
-    "21616083Q",
-    "26868974Y",
-    "40135330P",
-    "89044648X",
-    "80117501Z",
-    "34168723S",
-    "76857238R",
-    "66714505S",
-    "66499420A",
-]
+    print(tabla.getLetra(0))  # T
+    print(tabla.getLetra(22)) # E
+    print(tabla.getLetra(30)) # Excepcion!
 
-print("\n #### CASOS TEST DNI OK #### \n")
+    print("\n## LETRAS NO PERMITIDAS ##\n")
 
-for testString in CASOS_TEST_OK:
-    dni = Dni(testString)
-    print(dni.getDni())
-    dni.checkCIF()
-    # print("dni --->", dni.getNumeroSano())
-    # print("Letra --->", dni.getLetraSana())
-    # print("La letra es", dni.obtenerLetra())
-    prettyFormatter(dni.getNumeroSano(), dni.getDni())
-    prettyFormatter(dni.getLetraSana(), dni.obtenerLetra())
+    mostrarLetrasNoPermitidas(tabla)
+
+    ### Añado casos test INCORRECTOS ALEATORIOS ###
+
+    numero_casos = 15
+    cifs_letra_no_permitida = generarCIFLetraNoPermitida(numero_casos)
+
+    print("\n## CASOS TEST LETRA NO PERMITIDA ##\n")
+
+    print(cifs_letra_no_permitida)
+
+    for cif in cifs_letra_no_permitida:
+        if tabla.calcularLetra(cif[:-1]) == cif[-1]:
+            print(f"{cif} {Colors.OKGREEN.value} OK {Colors.ENDC.value}")
+        else:
+            # print("%s %s" % (dni, Colors.FAIL + "FAIL" + Colors.ENDC))
+            print(f"{cif} {Colors.FAIL.value} FAIL {Colors.ENDC.value}")
+
+
+    ### DNI ###
+
+    print("\n\n######     DNI     ######\n")
+
+    ### Casos test DNI ALEATORIOS ###
+
+    numero_casos = 25
+    cifs_aleatorios = generar_cifs_aleatorios(numero_casos)
+
+    print("\n## CASOS TEST DNI ALEATORIOS ##\n")
+
+    print(cifs_aleatorios)
+
+    for cif in cifs_aleatorios:
+        dni = Dni(cif)
+        print(dni.getDni())
+        dni.checkCIF()
+        # print("dni --->", dni.getNumeroSano())
+        # print("Letra --->", dni.getLetraSana())
+        # print("La letra es", dni.obtenerLetra())
+        prettyFormatter(dni.getNumeroSano(), dni.getDni())
+        prettyFormatter(dni.getLetraSana(), dni.obtenerLetra())
+
+    ### Casos test OK ###
+
+    CASOS_TEST_OK = [  # casos OK
+        "78484464T",
+        "72376173A",
+        "01817200Q",
+        "95882054E",
+        "63587725Q",
+        "26861694V",
+        "21616083Q",
+        "26868974Y",
+        "40135330P",
+        "89044648X",
+        "80117501Z",
+        "34168723S",
+        "76857238R",
+        "66714505S",
+        "66499420A",
+    ]
+
+    print("\n #### CASOS TEST DNI OK #### \n")
+
+    for cif_ok in CASOS_TEST_OK:
+        dni = Dni(cif_ok)
+        print(dni.getDni())
+        dni.checkCIF()
+        # print("dni --->", dni.getNumeroSano())
+        # print("Letra --->", dni.getLetraSana())
+        # print("La letra es", dni.obtenerLetra())
+        prettyFormatter(dni.getNumeroSano(), dni.getDni())
+        prettyFormatter(dni.getLetraSana(), dni.obtenerLetra())
+
+
+
+if __name__ == "__main__":
+    main()
